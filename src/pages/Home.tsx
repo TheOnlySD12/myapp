3af1fc0d44fb965a14e82336d68e9834c6d9bf12
TheOnlySD12 <helem.gyphon@gmail.com>
2026-02-17 11:03:36 +0200

    test tabel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
-import {IonContent, IonHeader, IonPage, IonText, IonTitle, IonToggle, IonToolbar} from '@ionic/react';
+import {IonAlert, IonButton, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar} from '@ionic/react';
 import './Home.css';
 import React from "react";
-import {useScanSettings} from "../Settings";
+
+const rainbowStyle = {
+    background: "linear-gradient(to right, #FF0000, #FF7F00, #FFFF00, #00FF00, #0000FF, #4B0082, #8F00FF)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent"
+}
+
 
 const Home: React.FC = () => {
-    const { scanMode, setScanMode } = useScanSettings();
-
     return (
         <IonPage>
             <IonHeader>
@@ -13,21 +17,41 @@ const Home: React.FC = () => {
                     <IonTitle>ElfScanner</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle style={{fontSize: "32px"}}>Home</IonTitle>
-                </IonToolbar>
-            </IonHeader>
             <IonContent scrollY={false} forceOverscroll={false}>
-                <IonText color="white">
-                    <p>Sara este <IonText color="danger">asa</IonText>.</p>
-                </IonText>
-                <IonToggle checked={scanMode === "battery"} onIonChange={(e) =>
-                    setScanMode(e.detail.checked ? "battery" : "instant")
-                }>
-                    Low Power Mode
-                </IonToggle>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle size="large">Home</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <p>
+                    <IonText>
+                         Sara este <IonText style={rainbowStyle}>asa</IonText>.
+                    </IonText>
+                </p>
+                <IonButton id="present-alert">Click Me</IonButton>
+                <IonAlert
+                    header="Alert!"
+                    trigger="present-alert"
+                    buttons={[
+                        {
+                            text: 'Cancel',
+                            role: 'cancel',
+                            handler: () => {
+                                console.log('Alert canceled');
+                            },
+                        },
+                        {
+                            text: 'OK',
+                            role: 'confirm',
+                            handler: () => {
+                                console.log('Alert confirmed');
+                            },
+                        },
+                    ]}
+                    onDidDismiss={({ detail }) => console.log(`Dismissed with role: ${detail.role}`)}
+                ></IonAlert>
             </IonContent>
+
         </IonPage>
     );
 };
